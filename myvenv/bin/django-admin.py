1978e089982c1ecea97506f374a018aff2e20981
djangoGirlsWindows <94135972+djangoGirlsWindows@users.noreply.github.com>
2021-11-11 21:19:49 +0100

    Delete myvenv directory
    Had to be in gitignore

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/annemarleen/Programming/DjangoGirls/November2021/djangogirls/myvenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
